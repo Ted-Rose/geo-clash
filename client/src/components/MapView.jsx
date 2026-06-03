b3bb071e99da3484327c1e2c4ae1ecf1858c66f4
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2026-06-03 15:57:06 +0300

    feat: switch map tiles from OpenStreetMap to Esri World Imagery satellite view

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -133,8 +133,8 @@ export default function MapView({
       className="absolute inset-0"
     >
       <TileLayer
-        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
         maxNativeZoom={19}
         maxZoom={22}
       />
--- client/src/snake/GameScreen.jsx
+++ client/src/snake/GameScreen.jsx
@@ -398,8 +398,8 @@ export default function SnakeGameScreen({
         ref={mapRef}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
           maxZoom={22}
           maxNativeZoom={19}
         />
